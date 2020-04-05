c3302379a81c6ed8e0783af656511bbd0f04a2df
Jason Keeling <s2jason@yahoo.com>
2020-04-05 22:55:06 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,13 +9,6 @@ def line(katz_deli)
       line_positions << "#{number}. #{customer}"
       number += 1 
     end
-    puts "The line is currently: "
+    puts "The line is currently: #{line_positions.join(" ")}"
   end
 end
-
-def take_a_number(katz_deli, name)
-  katz_deli.to_i 
-  puts "Welcome, #{name}. You are number #{katz_deli} in line"
-end
-
-another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] 
