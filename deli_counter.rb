1ca5cfabd80f15407e18df1fe3b8e7ffcabd140a
Jason Keeling <s2jason@yahoo.com>
2020-04-05 22:30:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli} in line"
 end
 
-def line
+def line(deli)
+  puts "#{deli}"
+  
   puts "The line is currently empty."
 end
