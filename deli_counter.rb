4ccbd910bb9fa527f7f7d86ca6a2a6cd7f4cef17
Jason Keeling <s2jason@yahoo.com>
2020-04-05 22:47:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
-def take_a_number(katz_deli, name)
-  katz_deli.to_i 
-  puts "Welcome, #{name}. You are number #{katz_deli} in line"
-end
-
 def line(store)
   puts 
   puts "The line is currently empty."
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.to_i 
+  puts "Welcome, #{name}. You are number #{katz_deli} in line"
 end
 
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"] 
