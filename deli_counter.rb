c0e1bdcc3bb577d4a9a7d5e8251e83aa6d546f3d
Jason Keeling <s2jason@yahoo.com>
2020-04-05 22:51:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,12 @@
 # Write your code here.
-def line(store)
-  puts 
-  puts "The line is currently empty."
+def line(katz_deli)
+  line_positions = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1 
+    katz_deli.each do |customer|
+      line_positions << "#{number}"
 end
 
 def take_a_number(katz_deli, name)
